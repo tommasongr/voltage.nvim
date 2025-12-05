98085a6f409c9eb186f153f92dfb3f11d284389d
Tommaso Negri <tommaso@hey.com>
2025-12-05 21:38:43 +0100

    Consolidate fg and dimmed to the accent tones

--- lua/voltage/init.lua
+++ lua/voltage/init.lua
@@ -1,7 +1,8 @@
 local M = {}
 
 M.config = {
-	accent = "blue"
+	accent = "blue",
+	transparent = false
 }
 
 function M.setup(opts)
@@ -14,46 +15,60 @@ function M.setup(opts)
 	vim.g.colors_name = "voltage"
 
 	local colors = {
-		bg = nil,
-		fg = "#f0f0f0",
-		dimmed = "#707070",
-		inverted = "#000000",
 		-- strings, numbers, etc
 		green = {
+			lightest = "#e6f3e8",
+			lighter = "#68766a",
 			light = "#99cda3",
 			regular = "#54dd7d",
 			dark    = "#1c3e24",
-			darker  = "#0c1a0f"
+			darker  = "#0c1a0f",
+			darkest = "#010502"
 		},
 		-- constants
 		purple = {
+			lightest = "#f7eaf5",
+			lighter = "#7a6d79",
 			light = "#dcabd6",
 			regular = "#fd8ff4",
 			dark    = "#472944",
-			darker  = "#1e111c"
+			darker  = "#1e111c",
+			darkest = "#060206"
 		},
 		-- comments
 		yellow = {
+			lightest = "#f4eee0",
+			lighter = "#777161",
 			light = "#d2bc82",
 			regular = "#ebb600",
 			dark    = "#41340a",
-			darker  = "#1b1505"
+			darker  = "#1b1505",
+			darkest = "#050300"
 		},
 		-- top-level definitions
 		blue = {
+			lightest = "#e0f2f6",
+			lighter = "#61767a",
 			light = "#7dccda",
 			regular = "#00d8f5",
 			dark    = "#003d46",
-			darker  = "#031a1e"
+			darker  = "#031a1e",
+			darkest = "#000406"
 		},
 		-- errors, spellbad, etc
 		red = {
+			lightest = "#fceae8",
+			lighter = "#806c6a",
 			light = "#edaaa4",
 			regular = "#ff2335",
 			dark    = "#502825",
-			darker  = "#22100f"
+			darker  = "#22100f",
+			darkest = "#080202"
 		},
 	}
+
+	colors.fg = colors[M.config.accent].lightest
+	colors.dimmed = colors[M.config.accent].lighter
 
 	for _, highlight in ipairs({ "editor", "syntax", "treesitter" }) do
 		local mod = require("voltage.highlights." .. highlight)
